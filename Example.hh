2a21c13cc24e6a2f7f60d195c68cdd31197eb80a
steelbrain <aneesiqbalbhatti@gmail.com>
2015-04-05 08:06:27 -0700

    :art: Consume ListenInCoop in Example

--- Example.hh
+++ Example.hh
@@ -1,8 +1,23 @@
 <?hh
 require(__DIR__.'/Server.hh');
-$Server = new Server('localhost', 9098);
-$Server->Listen(async function(resource $Client):Awaitable<void>{
-  await Server::OnResponse($Client, async function(string $Data, resource $Client):Awaitable<void>{
-    echo "Client Sent Data: $Data\n";
-  });
-})->getWaitHandle()->join();
+class MyServerSidedApp{
+  public Server $Server;
+  public function __construct(){
+    $this->Server = $Server = new Server('localhost', 9001);
+  }
+  public async function OnConnect(resource $Client):Awaitable<void>{
+    await Server::OnResponse($Client, async function(string $Data, resource $Client){
+      echo "Client Sent Data: $Data\n";
+    });
+  }
+  public async function Init():Awaitable<void>{
+    await $this->Server->ListenInCoop(inst_meth($this, 'OnConnect'), async function(Server $Instance):Awaitable<void>{
+      $ShouldClose = rand(0, 1);
+      if($ShouldClose === 1){
+        $Instance->Close();
+      }
+    });
+  }
+}
+$App = new MyServerSidedApp();
+$App->Init()->getWaitHandle()->join();
