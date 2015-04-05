91d9cdaa353c532d0878c05eda40d194e088f870
steelbrain <aneesiqbalbhatti@gmail.com>
2015-04-05 00:41:32 -0700

    :construction: Await Page Process Requests and consume stream_select

--- Server.hh
+++ Server.hh
@@ -2,23 +2,34 @@
 class Server{
   public function __construct(public string $Address, public int $Port){}
   public async function Listen((function(resource):Awaitable<void>) $Callback):Awaitable<void>{
+    await RescheduleWaitHandle::create(RescheduleWaitHandle::QUEUE_DEFAULT,0);
     $Socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
     socket_bind($Socket, $this->Address, $this->Port);
-    socket_listen($Socket, 0);
-    register_shutdown_function(function() use($Socket){
+    socket_listen($Socket, 100);
+    register_shutdown_function(function() use ($Socket){
       socket_close($Socket);
     });
+    $Handles = Vector{};
     while(is_resource($Socket)){
-      $Callback(socket_accept($Socket))->getWaitHandle();
+      $Handles->add($Callback(socket_accept($Socket))->getWaitHandle());
+      if($Handles->count() === 20){
+        await GenVectorWaitHandle::create($Handles);
+        $Handles->clear();
+      }
     }
   }
-  public static async function OnResponse(resource $Client, (function(string, resource):Awaitable<void>) $Callback):Awaitable<void>{
-    while(is_resource($Client) && !feof($Client)){
-      $Content = fgets($Client);
-      if(is_string($Content)){
-        await $Callback($Content, $Client);
-      } else break;
+  public static async function OnResponse(resource $Socket, (function(string, resource):Awaitable<void>) $Callback):Awaitable<void>{
+    while (is_resource($Socket) && !feof($Socket)){
+      $select = await stream_await($Socket, STREAM_AWAIT_READ, 5);
+      switch ($select) {
+        case STREAM_AWAIT_READY:
+          await $Callback(fgets($Socket), $Socket);
+          break;
+        case STREAM_AWAIT_CLOSED:
+        return ;
+        // Do nothing for timeouts
+        default:
+      }
     }
-    socket_close($Client);
   }
 }
